741e7b6ef1f6cff6381f29235c3250f2cad1f04d
Héctor de la Rosa Prado <hoftherose@gmail.com>
2025-08-05 00:04:21 -0600

    Arreglar handler no es objeto

--- src/sigic_geonode/services/csv_service.py
+++ src/sigic_geonode/services/csv_service.py
@@ -1,8 +1,11 @@
-class CSVService():
+from sigic_geonode.services.csv_handler import CSVServiceHandler
+
+class CSVServiceInfo():
     services_type = {
         "CSV": {
             "OWS": False,
-            "handler": "sigic_geonode.services.csv_handler.CSVServiceHandler",
+            # "handler": "sigic_geonode.services.csv_handler.CSVServiceHandler",
+            "handler": CSVServiceHandler,
             "label": "CSV Service",
             "management_view": "sigic_geonode.services.csv_view.CSVView"
         }
