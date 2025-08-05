80e856864ec03c7c4f7b4887f510c3d5ad64a0eb
Héctor de la Rosa Prado <hoftherose@gmail.com>
2025-08-05 00:04:23 -0600

    Logica incompleto de harvester

--- src/sigic_geonode/services/csv_harvester.py
+++ src/sigic_geonode/services/csv_harvester.py
@@ -1,10 +1,28 @@
+import csv
+import requests
+from functools import lru_cache
+
 from geonode.harvesting.harvesters.base import BaseHarvesterWorker
+from geonode.base.models import ResourceBase
+from geonode.layers.models import Dataset
 
 class CSVHarvester(BaseHarvesterWorker):
+    http_session: requests.Session
+
+    def __init__(self, remote_url: str, harvester_id: int):
+        super().__init__(remote_url, harvester_id)
+        self.http_session = requests.Session()
+
+    def allows_copying_resources(self):
+        return False
+
+    def get_geonode_resource_type(self, _):
+        return Dataset
+
     @classmethod
     def from_django_record(cls, harvester: "Harvester"):
         # inicializar desde record de django
-        pass
+        return cls(remote_url=harvester.remote_url, harvester_id=harvester.id)
 
     def get_num_available_resources(self):
         # Regresar cuantos recursos
@@ -12,12 +30,22 @@ class CSVHarvester(BaseHarvesterWorker):
 
     def list_resources(self, offset = 0):
         # regresar lista de recursos
-        []
+        return [self.get_resource()]
 
     def check_availability(self, timeout_seconds = 5):
         return True
 
     def get_resource(self, harvestable_resource: "HarvestableResource"):
         #return HarvestedResourceInfo
-        pass
+        download = self.http_session.get(harvestable_resource.unique_identifier)
+
+        if download.status_code == requests.codes.ok:
+
+        decoded_content = download.content.decode('utf-8')
+        return csv.reader(decoded_content.splitlines(), delimiter=',')
+
+@lru_cache
+def CSVParser(url):
+    response = urllib2.urlopen(url)
+    return csv.reader(response)
 
