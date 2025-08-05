00a410f3d157a85b2c6e64a3cae4313e9c885eaa
Héctor de la Rosa Prado <hoftherose@gmail.com>
2025-08-05 00:04:18 -0600

    Formatear objeto de servicio

--- src/sigic_geonode/services/csv_service.py
+++ src/sigic_geonode/services/csv_service.py
@@ -1,5 +1,11 @@
-# from importer.handlers.csv.handler import ImporterViewSet
-
 class CSVService():
-    services_type = {"CSV": {"OWS": False, "handler": "importer.handlers.csv.handler.CSVFileHandler", "label": "CSV Test", "management_view": "sigic_geonode.services.csv_view.CSVView"}}
+    services_type = {
+        "CSV": {
+            "OWS": False,
+            "handler": "sigic_geonode.services.csv_handler.CSVServiceHandler",
+            "label": "CSV Test",
+            "management_view": "sigic_geonode.services.csv_view.CSVView"
+        }
+    }
     # services_type = {"CSV": {"OWS": False, "handler": "CSVFileHandler", "label": "CSV Test", "management_view": "sigic_geonode.services.csv_view.CSVView"}}
+
