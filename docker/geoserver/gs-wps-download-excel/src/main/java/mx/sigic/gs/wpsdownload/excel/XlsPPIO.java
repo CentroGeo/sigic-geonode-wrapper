9cccfb8890e4f50ac1d123f8bcdca9b059aa5418
Niji <mathereall@gmail.com>
2025-08-15 03:39:07 -0600

    Refactor XlsPPIO and XlsxPPIO to dynamically extract headers using reflection and improve error handling

--- docker/geoserver/gs-wps-download-excel/src/main/java/mx/sigic/gs/wpsdownload/excel/XlsPPIO.java
+++ docker/geoserver/gs-wps-download-excel/src/main/java/mx/sigic/gs/wpsdownload/excel/XlsPPIO.java
@@ -1,64 +1,124 @@
-package mx.sigic.wps.excel;
+package mx.sigic.gs.wpsdownload.excel;
 
-import java.io.OutputStream;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;  // XLS
-import org.apache.poi.ss.usermodel.*;
 import org.geoserver.wps.ppio.ComplexPPIO;
-import org.geotools.api.data.simple.SimpleFeatureCollection;
-import org.geotools.api.data.simple.SimpleFeatureIterator;
-import org.geotools.api.feature.simple.SimpleFeature;
-import org.geotools.api.feature.simple.SimpleFeatureType;
-import org.geotools.api.feature.type.AttributeDescriptor;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.FeatureIterator;
+
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.WKTWriter;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook; // XLS
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XlsPPIO extends ComplexPPIO {
 
+    public static final String MIME_XLS = "application/vnd.ms-excel";
+
     public XlsPPIO() {
-        super(OutputStream.class, SimpleFeatureCollection.class,
-              "application/vnd.ms-excel");
+        super(OutputStream.class, SimpleFeatureCollection.class, MIME_XLS);
     }
 
     @Override
     public void encode(Object value, OutputStream os) throws Exception {
         SimpleFeatureCollection fc = (SimpleFeatureCollection) value;
+
         try (Workbook wb = new HSSFWorkbook()) {
             Sheet sh = wb.createSheet("data");
-            SimpleFeatureType ft = fc.getSchema();
+            WKTWriter wkt = new WKTWriter();
 
-            Row header = sh.createRow(0);
-            for (int i = 0; i < ft.getAttributeCount(); i++) {
-                AttributeDescriptor ad = ft.getDescriptor(i);
-                header.createCell(i).setCellValue(ad.getLocalName());
-            }
+            List<String> headers = null;
+            int r = 0;
 
-            int r = 1;
-            try (SimpleFeatureIterator it = fc.features()) {
+            try (FeatureIterator<?> it = fc.features()) {
                 while (it.hasNext()) {
-                    SimpleFeature f = it.next();
+                    Object f = it.next();
+
+                    if (headers == null) {
+                        headers = extractHeadersFromFeature(f);
+                        Row header = sh.createRow(r++);
+                        for (int c = 0; c < headers.size(); c++) {
+                            header.createCell(c).setCellValue(headers.get(c));
+                        }
+                    }
+
                     Row row = sh.createRow(r++);
-                    for (int i = 0; i < ft.getAttributeCount(); i++) {
-                        Object v = f.getAttribute(i);
-                        Cell c = row.createCell(i);
-                        if (v == null) { c.setBlank(); continue; }
-                        if (v instanceof Number) {
-                            c.setCellValue(((Number) v).doubleValue());
+                    for (int c = 0; c < headers.size(); c++) {
+                        Object v = getAttributeByIndex(f, c);
+                        Cell cell = row.createCell(c);
+                        if (v == null) {
+                            cell.setBlank();
+                        } else if (v instanceof Number) {
+                            cell.setCellValue(((Number) v).doubleValue());
                         } else if (v instanceof java.util.Date) {
-                            c.setCellValue((java.util.Date) v);
+                            cell.setCellValue((java.util.Date) v);
                         } else if (v instanceof Geometry) {
-                            c.setCellValue(((Geometry) v).toText());
+                            cell.setCellValue(wkt.write((Geometry) v));
                         } else {
-                            c.setCellValue(String.valueOf(v));
+                            cell.setCellValue(String.valueOf(v));
                         }
                     }
                 }
             }
+
+            if (headers != null) {
+                for (int c = 0; c < Math.min(headers.size(), 50); c++) {
+                    sh.autoSizeColumn(c);
+                }
+            }
+
             wb.write(os);
+        } catch (RuntimeException e) {
+            throw new IOException("Error generando XLS", e);
         }
+    }
+
+    @Override
+    public Object decode(InputStream input) throws Exception {
+        throw new UnsupportedOperationException("XLS decode no soportado");
     }
 
     @Override
     public String getFileExtension() { return "xls"; }
 
-    @Override
-    public PPIODirection getDirection() { return PPIODirection.ENCODING; }
+    // ===== Helpers reflexión =====
+
+    @SuppressWarnings("unchecked")
+    private static List<String> extractHeadersFromFeature(Object feature) {
+        try {
+            Object ft = feature.getClass().getMethod("getFeatureType").invoke(feature);
+            Method getAttrDesc = ft.getClass().getMethod("getAttributeDescriptors");
+            List<Object> descriptors = (List<Object>) getAttrDesc.invoke(ft);
+
+            List<String> headers = new ArrayList<>(descriptors.size());
+            for (Object d : descriptors) {
+                Object name = d.getClass().getMethod("getName").invoke(d);
+                String col;
+                try {
+                    col = (String) name.getClass().getMethod("getLocalPart").invoke(name);
+                } catch (NoSuchMethodException nsme) {
+                    col = String.valueOf(name);
+                }
+                headers.add(col);
+            }
+            return headers;
+        } catch (Exception e) {
+            throw new RuntimeException("No pude leer encabezados vía reflexión", e);
+        }
+    }
+
+    private static Object getAttributeByIndex(Object feature, int index) {
+        try {
+            return feature.getClass().getMethod("getAttribute", int.class).invoke(feature, index);
+        } catch (Exception e) {
+            throw new RuntimeException("No pude leer atributo[" + index + "]", e);
+        }
+    }
 }
