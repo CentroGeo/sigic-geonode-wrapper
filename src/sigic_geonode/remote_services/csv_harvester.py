1df398e902159e513be516fb73b49a7e4dca4119
Héctor de la Rosa Prado <hoftherose@gmail.com>
2025-08-17 03:44:42 -0600

    Limpieza general de csv_harvester

--- src/sigic_geonode/remote_services/csv_harvester.py
+++ src/sigic_geonode/remote_services/csv_harvester.py
@@ -1,33 +1,32 @@
 import os
-import io
 import requests
 import typing
-import logging
-from functools import lru_cache
-from dataclasses import dataclass, field
+
 from uuid import uuid4
+from pathlib import Path
 from datetime import date
 from importlib import import_module
-from pathlib import Path
+from functools import lru_cache
+from dataclasses import dataclass, field
 
-from django.utils.text import slugify
-from django.core.files import uploadedfile
 from django.core.files.uploadedfile import UploadedFile
-from django.contrib.gis.geos import Polygon
 from django.conf import settings
+from django.contrib.gis.geos import Polygon
 from django.http import HttpRequest
+from django.utils.text import slugify
 
 from geonode.harvesting.resourcedescriptor import RecordDescription, RecordIdentification, RecordDistribution
-from geonode.harvesting.harvesters.base import BaseHarvesterWorker, download_resource_file
-from geonode.harvesting import config
+from geonode.harvesting.harvesters.base import BaseHarvesterWorker
 from geonode.resource.manager import resource_manager
 from geonode.storage.manager import storage_manager
 from geonode.layers.models import Dataset
 from geonode.base.auth import get_or_create_token
 
 from importer.api.views import ImporterViewSet
+
 engine = import_module(settings.SESSION_ENGINE)
-logger = logging.getLogger(__name__)
+
+MEXICO_SPATIAL_EXTENT = Polygon(((-122.19, 12.1),(-122.19, 32.72),(-84.64, 32.72),(-84.64, 12.1),(-122.19, 12.1)))
 
 @dataclass()
 class CSVRemoteData:
@@ -63,15 +62,14 @@ class CSVHarvester(BaseHarvesterWorker):
 
     @classmethod
     def from_django_record(cls, harvester: "Harvester"):
-        # inicializar desde record de django
         return cls(remote_url=harvester.remote_url, harvester_id=harvester.id)
 
     def get_num_available_resources(self):
-        # Regresar cuantos recursos
+        # TODO Agregar funcionalidad en caso de tener archivos zip
         return 1
 
     def list_resources(self, offset = 0):
-        # regresar lista de recursos
+        # TODO Agregar funcionalidad en caso de tener archivos zip
         return [
             CSVRemoteData(
                 unique_identifier = self.uuid,
@@ -82,6 +80,7 @@ class CSVHarvester(BaseHarvesterWorker):
         ]
 
     def check_availability(self, timeout_seconds = 5):
+        # TODO Agregar ping para revisar que si hay servidor
         return True
 
     def get_resource(self, harvestable_resource: "HarvestableResource"):
@@ -89,20 +88,23 @@ class CSVHarvester(BaseHarvesterWorker):
             resource_descriptor=RecordDescription(
                 uuid=harvestable_resource.unique_identifier,
                 identification=RecordIdentification(
+                    # TODO Name según valores entrada
                     name=self.name,
+                    # TODO Title según valores entrada
                     title=self.title,
                     date=str(date.today()),
                     date_type="upload",
+                    # TODO Abstract según valores entrada
                     abstract="",
+                    # TODO Purpose según valores entrada
                     purpose="",
-                    # TODO Extremos de datos geo
-                    spatial_extent=Polygon(
-                        ((-122.19, 12.1),(-122.19, 32.72),(-84.64, 32.72),(-84.64, 12.1),(-122.19, 12.1))
-                    ),
+                    # TODO Extremos de datos geo según los datos
+                    spatial_extent=MEXICO_SPATIAL_EXTENT,
                     other_constraints="",
                     other_keywords=[],
                     supplemental_information="",
                 ),
+                # TODO Language según valores entrada
                 language="spa",
                 data_quality="",
                 distribution=RecordDistribution(
@@ -131,14 +133,6 @@ class CSVHarvester(BaseHarvesterWorker):
         else:
             geonode_resource = self._update_existing_geonode_resource(geonode_resource, defaults)
         
-        # with transaction.atomic():
-        #     geoserver_resource = GeoServerResourceManager().create(
-        #         defaults["uuid"],
-        #         resource_type=geonode_resource_type,
-        #         defaults=defaults,
-        #     )
-        #     geoserver_resource.save(notify=False)
-        #     geonode_resource.save(notify=False)
         harvestable_resource.geonode_resource = geonode_resource
         harvestable_resource.save()
 
@@ -164,13 +158,11 @@ class CSVHarvester(BaseHarvesterWorker):
         }
         defaults["name"] = harvested_resource_info.resource_descriptor.identification.name
         defaults.update(harvested_resource_info.resource_descriptor.additional_parameters)
-        # return {key: value for key, value in defaults.items() if value is not None}
         return defaults
 
     def _create_new_geonode_resource(self, geonode_resource_type, defaults):
-        # file_path = download_resource_file(self.url, target_name=self.name+".csv")
         file = download_to_geonode(self.url, target_name=os.getenv("MEDIA_ROOT")+self.name+".csv")
-        setup_importer(defaults, file)
+        create_from_importer(defaults, file)
 
         geonode_resource = resource_manager.create(
             defaults["uuid"],
@@ -194,22 +186,6 @@ def CSVParser(url: str):
     return fn
 
 def download_to_geonode(url: str, target_name: str):
-    # query_params = {"downloadformat": "csv"}
-    # response = requests.get(url, params=query_params)
-    # response.raise_for_status()
-    # file_size = response.headers.get("Content-Length")
-    # content_type = response.headers.get("Content-Type")
-    # charset = response.apparent_encoding
-    # size_threshold = config.get_setting("HARVESTED_RESOURCE_FILE_MAX_MEMORY_SIZE")
-    # if file_size is not None and int(file_size) < size_threshold:
-    #     logger.debug("Downloading to an in-memory buffer...")
-    #     buf = io.BytesIO()
-    #     file_ = uploadedfile.InMemoryUploadedFile(buf, None, target_name, content_type, file_size, charset)
-    # else:
-    #     logger.debug("Downloading to a temporary file...")
-    #     file_ = uploadedfile.TemporaryUploadedFile(target_name, content_type, file_size, charset)
-    # return file_
-
     query_params = {"downloadformat": "csv"}
     response = requests.get(url, params=query_params)
     target_name = slugify(url)+".csv"
@@ -223,35 +199,10 @@ def download_to_geonode(url: str, target_name: str):
         if storage_manager.exists(target_name):
             storage_manager.delete(target_name)
         file_name = storage_manager.save(target_name, file)
-        result = Path(storage_manager.path(file_name))
-    return UploadedFile(open(result, mode="rb"), result.name, content_type=content_type, size=file_size, charset=charset)
+        path = Path(storage_manager.path(file_name))
+    return UploadedFile(open(path, mode="rb"), result.name, content_type=content_type, size=file_size, charset=charset)
 
-def setup_importer(defaults, file):
-    # logger = logging.getLogger(__name__)
-    # files={
-    #     "base_file": (defaults["files"].name, open(str(defaults["files"].absolute(), mode="rb"))),
-    #     "charset": (None, "UTF-8"),
-    #     "store_spatial_files": (None, True),
-    #     "Time": (None, True),
-    # }
-    # cookies = {
-    #     'JSESSIONID': '19C56F6CF3CFA0ABF5941441F1726D8D',
-    #     'csrftoken': 'sBZLZzrdF5CTDwmhPw1zEJjBe9fcSp8q',
-    #     'sessionid': 'h4zzq9w252q95puj340i5vtgpe6g073q',
-    #     'tabstyle': 'html-tab',
-    # }
-    # headers = {
-    #         'Accept': 'application/json, text/plain, */*',
-    #         }
-    # response = requests.post(
-    #     reverse('ImporterViewSet'),
-    #     files=files,
-    #     cookies=cookies,
-    #     headers=headers,
-    # )
-    # logger.error(str(response.request.data))
-    # logger.error(str(response.headers))
-    # logger.error(str(response.))
+def create_from_importer(defaults, file):
     request = HttpRequest()
     request.method = "POST"
     request.user = defaults["owner"]
