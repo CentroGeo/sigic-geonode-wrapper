199e1b1cae46e227cf233da4978326ef3593e1ad
Niji <mathereall@gmail.com>
2025-08-29 02:31:29 -0600

    Enhance OIDC token handling by updating authentication methods in apps.py and modifying INSTALLED_APPS in settings.py

--- src/sigic_geonode/sigic_auth/apps.py
+++ src/sigic_geonode/sigic_auth/apps.py
@@ -22,3 +22,17 @@ class SigicAuthConfig(AppConfig):
         from .patches import patch_get_token_from_auth_header
         patch_get_token_from_auth_header()
         print("[geonode_keycloak] OIDC habilitado. Parche get_token_from_auth_header() aplicado.")
+
+        import geonode.base.auth as gba
+
+        import geonode.api.authentication as geonode_api_authentication
+        geonode_api_authentication.get_token_from_auth_header = gba.get_token_from_auth_header
+
+        import geonode.proxy.views as geonode_proxy_views
+        geonode_proxy_views.get_token_from_auth = gba.get_token_from_auth_header
+
+        import geonode.security.middleware as geonode_security_middleware
+        geonode_security_middleware.get_token_from_auth = gba.get_token_from_auth_header
+
+        import geonode.utils as geonode_utils
+        geonode_utils.get_token_from_auth_header = gba.get_token_from_auth_header
