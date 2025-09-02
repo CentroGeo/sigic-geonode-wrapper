d81715a68c8fde0f6f9bcf44684e1a4bf0f5b890
Sergio <sage_sergio@hotmail.com>
2025-09-02 14:37:58 -0600

    Se agrego una nueva api para obtener la imagen previamente cargada por post

--- src/sigic_geonode/media_uploads/urls.py
+++ src/sigic_geonode/media_uploads/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import upload_image_preview
+from .views import upload_image_preview, upload_status
 
 urlpatterns = [
     path("ia", upload_image_preview, name="upload-project-preview"),
+    path("ia/register", upload_status, name="register-project-preview"),
 ]
