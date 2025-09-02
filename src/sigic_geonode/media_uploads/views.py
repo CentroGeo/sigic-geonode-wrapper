6a483b1146243bdd4c06726c02a65c87f6fb839b
Sergio <sage_sergio@hotmail.com>
2025-09-02 14:37:58 -0600

    Se agrego una nueva api para obtener la imagen previamente cargada por post

--- src/sigic_geonode/media_uploads/views.py
+++ src/sigic_geonode/media_uploads/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import api_view, parser_classes
 import os
+import mimetypes
 
 @api_view(["GET", "POST"])
 @parser_classes([MultiPartParser, FormParser])
@@ -22,9 +23,9 @@ def upload_image_preview(request):
         filename = f"{timezone.now().strftime('%Y%m%d%H%M%S')}_{file.name}"
         
         if(category == "proyectos"):
-            upload_dir = os.path.join(settings.MEDIA_ROOT, "IA", "uploads", "proyectos")
+            upload_dir = os.path.join(settings.MEDIA_ROOT, "IA", "uploads", "projects")
         else:
-            upload_dir = os.path.join(settings.MEDIA_ROOT, "IA", "uploads", "contextos")
+            upload_dir = os.path.join(settings.MEDIA_ROOT, "IA", "uploads", "contexts")
             
         os.makedirs(upload_dir, exist_ok=True)
         save_path = os.path.join(upload_dir, filename)
@@ -38,3 +39,20 @@ def upload_image_preview(request):
             "message": "File uploaded successfully",
             "url": f"{settings.MEDIA_URL}{filename}"
         })
+
+@api_view(["GET", "POST"])
+def upload_status(request):
+    if request.method == 'POST':
+        filename = request.POST.get("filename")
+        
+        previews_dir = os.path.join(settings.MEDIA_ROOT, "IA", "uploads", "contexts")
+        file_path = os.path.join(previews_dir, filename)
+
+        if not os.path.exists(file_path):
+            raise Http404("Archivo no encontrado")
+
+        mime_type, _ = mimetypes.guess_type(file_path)
+        if not mime_type:
+            mime_type = "application/octet-stream"
+
+        return FileResponse(open(file_path, "rb"), content_type=mime_type)
