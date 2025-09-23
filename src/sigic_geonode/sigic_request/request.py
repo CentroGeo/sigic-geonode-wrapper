bd585862dd53135919c2581525f6260ad9ac0a90
andres-geotec <andres.geotec@gmail.com>
2025-09-23 15:10:14 -0600

    definiendo rutas posibles de solicitud de aprobación

--- src/sigic_geonode/sigic_request/request.py
+++ src/sigic_geonode/sigic_request/request.py
@@ -1,14 +1,34 @@
+import logging
+
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-class Test(APIView):
+logger = logging.getLogger(__name__)
+
+class Index(APIView):
     def get(self, request: Request):
+
+        return Response({
+            "status": "Index success",
+        })
+
+class Approval(APIView):
+    def post(self, request: Request):
         # Requiere: user, recurso
         request_data: dict[str, str] = request.data
-
+        
         return Response({
             "status": "success",
             "user": request_data.get("user", ""),
             "recurso": request_data.get("recurso", ""),
+            "msg": request_data.get("msg", ""),
+        })
+
+class Test(APIView):
+    # ''
+    def get(self, request: Request):
+
+        return Response({
+            "status": "Test success",
         })
--- src/sigic_geonode/sigic_request/urls.py
+++ src/sigic_geonode/sigic_request/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from sigic_geonode.sigic_request.request import Test
+from sigic_geonode.sigic_request.request import Index, Approval, Test
 
 urlpatterns = [
+    path("/", Index.as_view(), name="index"),
     path("/test", Test.as_view(), name="test"),
+    path("/approval", Approval.as_view(), name="approval"),
+    # path("/approval/pk", Test.as_view(), name="approval-detail"),
+    path("/publication", Test.as_view(), name="publication"),
+    # path("/publication/pk", Test.as_view(), name="publication-detail"),
 ]
