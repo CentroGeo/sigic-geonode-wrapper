18acfbd9e3e0d5235e218019c967d23334c985dd
Fernando Valle <fersoriano@ciencias.una.mx>
2025-09-23 18:05:18 -0600

    se agregan las urls de sigic_resources

--- src/sigic_geonode/urls.py
+++ src/sigic_geonode/urls.py
@@ -18,13 +18,14 @@
 #
 #########################################################################
 
-from django.urls import path, include
+from django.urls import include, path
 from geonode.urls import urlpatterns as geonode_urlpatterns
+
 from sigic_geonode.sigic_auth.debug import whoami
 
 urlpatterns = [
     path("sigic/whoami", whoami),
     path("sigic/georeference", include("sigic_geonode.sigic_georeference.urls")),
     path("sigic/ia/mediauploads/", include("sigic_geonode.sigic_ia_media_uploads.urls")),
+    path("api/v2/", include("sigic_geonode.sigic_resources.urls")),
 ] + geonode_urlpatterns
-
