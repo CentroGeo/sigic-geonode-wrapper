d28a057a1435ce248a9917d0d0823bda367cf407
Fernando Valle <fersoriano@ciencias.una.mx>
2025-09-23 18:06:29 -0600

    se agregan las rutas dedlos viewsets: sigic-resources y sigic-resources-short

---
+++ src/sigic_geonode/sigic_resources/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import SigicResourceBaseViewSet, SigicResourceShortViewSet
+
+# from .views import SigicResourceBaseViewSet
+
+
+router = DefaultRouter()
+router.register(
+    r"sigic-resources", SigicResourceBaseViewSet, basename="sigic-resources"
+)
+router.register(
+    r"sigic-resources-short",
+    SigicResourceShortViewSet,
+    basename="sigic-resources-short",
+)
+# la url :/api/v2/resources/ mantiene la lógica original de ResourceBaseViewSet
+urlpatterns = [
+    path("", include(router.urls)),
+]
