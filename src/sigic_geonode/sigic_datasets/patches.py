14ddc556c10c4bea6f05538318d04eca1a1766b6
andres-geotec <andres.geotec@gmail.com>
2025-10-10 18:53:44 -0600

    feat(metadata attribute): valida que no se modifique un atributo que no es de la capa

--- src/sigic_geonode/sigic_datasets/patches.py
+++ src/sigic_geonode/sigic_datasets/patches.py
@@ -40,33 +40,33 @@ if not getattr(DatasetViewSet, "_patched_by_monkey", False):
 
     # Modifica los metadatos de los atributos existentes en la capa
     def change_attribute_set(self, request_data):
+        logger.debug("🚀🚀 change_attribute_set ejecutado")
+        print("🚀🚀🔄 change_attribute_set ejecutado")
+
         request_data_dict = request_data_to_dict(request_data)
         request_attributes = request_data_dict.get("attribute_set")
 
         if request_attributes == None:
             return
         
-        dataset = self.get_object()
-        print("🔄 custom_partial_update - dataset:", dataset)
-        # print("🔄 custom_partial_update - attribute_set:", dataset.get("attribute_set"))
-        
         request_attributes_dic = eval(request_attributes)
-        for pk_attribute in request_attributes_dic.keys():
-            # TODO: validar que no se modifiquen atributos ue no pertenezcan a la capa
-            # if attribute.get(metadata_field) == None:
-            #     continue
+        request_attributes_dic_keys = request_attributes_dic.keys()
+        # print("🔄 custom_partial_update - request_attributes_dic_keys:", request_attributes_dic_keys)
 
-            request_attribute = request_attributes_dic.get(pk_attribute)
-            geonpode_attribute = Attribute.objects.get(pk=pk_attribute)
+        for geonode_attribute in Attribute.objects.filter(dataset=self.get_object()):
+            if (str(geonode_attribute.pk) in request_attributes_dic_keys) == False:
+                continue
 
-            if request_attribute.get("attribute_label"):
-                geonpode_attribute.attribute_label = request_attribute.get("attribute_label")
-            if request_attribute.get("description"):
-                geonpode_attribute.description = request_attribute.get("description")
-            if request_attribute.get("display_order"):
-                geonpode_attribute.display_order = request_attribute.get("display_order")
-            if request_attribute.get("visible"):
-                geonpode_attribute.visible = request_attribute.get("visible")
+            request_attribute = request_attributes_dic.get(str(geonode_attribute.pk))
+
+            if request_attribute.get("attribute_label") != None:
+                geonode_attribute.attribute_label = request_attribute.get("attribute_label")
+            if request_attribute.get("description") != None:
+                geonode_attribute.description = request_attribute.get("description")
+            if request_attribute.get("display_order") != None:
+                geonode_attribute.display_order = request_attribute.get("display_order")
+            if request_attribute.get("visible") != None:
+                geonode_attribute.visible = request_attribute.get("visible")
             
             # for metadata_field in METADATA_FIELDS:
             #     if attribute.get(metadata_field) == None:
@@ -74,12 +74,12 @@ if not getattr(DatasetViewSet, "_patched_by_monkey", False):
 
             #     print("🔄 custom_partial_update - metadata_field", metadata_field, attribute.get(metadata_field))
             #     attr[metadata_field] = attribute.get(metadata_field)
-                
-            geonpode_attribute.save()
+
+            geonode_attribute.save()
     
     _orig_partial_update = DatasetViewSet.partial_update
     def custom_partial_update(self, request, *args, **kwargs):
-        print("🔄 custom_partial_update - request", request.data)
+        # print("🔄 custom_partial_update - request", request.data)
         change_attribute_set(self, request.data)
 
         return _orig_partial_update(self, request, *args, **kwargs)
