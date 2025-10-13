ce92412735ae217497f34b19066c6c3b23b54bf6
andres-geotec <andres.geotec@gmail.com>
2025-10-13 16:59:32 -0600

    fix(metadata attribute): quita el uso de eval e == cuando se puede usar is o not

--- src/sigic_geonode/sigic_datasets/patches.py
+++ src/sigic_geonode/sigic_datasets/patches.py
@@ -1,9 +1,10 @@
 import logging
+import json
 
 from django.utils.datastructures import MultiValueDict
 from geonode.layers.api.views import DatasetViewSet
 from geonode.layers.models import Attribute
-from rest_framework.response import Response
+# from rest_framework.response import Response
 
 
 logger = logging.getLogger(__name__)
@@ -41,35 +42,43 @@ if not getattr(DatasetViewSet, "_patched_by_monkey", False):
     # Modifica los metadatos de los atributos existentes en la capa
     def change_attribute_set(self, request_data):
         logger.debug("🚀🚀 change_attribute_set ejecutado")
-        print("🚀🚀🔄 change_attribute_set ejecutado")
 
         request_data_dict = request_data_to_dict(request_data)
         request_attributes = request_data_dict.get("attribute_set")
 
-        if request_attributes == None:
+        if request_attributes is None:
             return
         
-        request_attributes_dic = eval(request_attributes)
+        # request_attributes_dic = eval(request_attributes)
+        request_attributes_dic = json.loads(request_attributes)
         request_attributes_dic_keys = request_attributes_dic.keys()
         # print("🔄 custom_partial_update - request_attributes_dic_keys:", request_attributes_dic_keys)
 
         for geonode_attribute in Attribute.objects.filter(dataset=self.get_object()):
-            if (str(geonode_attribute.pk) in request_attributes_dic_keys) == False:
+            if (str(geonode_attribute.pk) not in request_attributes_dic_keys):
                 continue
 
             request_attribute = request_attributes_dic.get(str(geonode_attribute.pk))
 
-            if request_attribute.get("attribute_label") != None:
+            if request_attribute.get("attribute_label") is not None:
                 geonode_attribute.attribute_label = request_attribute.get("attribute_label")
-            if request_attribute.get("description") != None:
+
+            if request_attribute.get("description") is not None:
                 geonode_attribute.description = request_attribute.get("description")
-            if request_attribute.get("display_order") != None:
+
+            if request_attribute.get("display_order") is not None:
                 geonode_attribute.display_order = request_attribute.get("display_order")
-            if request_attribute.get("visible") != None:
-                geonode_attribute.visible = request_attribute.get("visible")
+
+            if request_attribute.get("visible") is not None:
+                request_attribute_visible = request_attribute.get("visible")
+                print("🔄 custom_partial_update - json.loads.visible", request_attribute_visible)
+                if isinstance(request_attribute_visible, bool):
+                    geonode_attribute.visible = request_attribute_visible
+                elif isinstance(request_attribute_visible, str):
+                    geonode_attribute.visible = request_attribute_visible.lower() == "true"
             
             # for metadata_field in METADATA_FIELDS:
-            #     if attribute.get(metadata_field) == None:
+            #     if attribute.get(metadata_field) is None:
             #         continue
 
             #     print("🔄 custom_partial_update - metadata_field", metadata_field, attribute.get(metadata_field))
