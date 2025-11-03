6abe00fcfa645730db96bb2ceabbec4d1aa8bb53
Niji <mathereall@gmail.com>
2025-11-03 02:39:10 -0600

    fix: enhance username retrieval logic in account adapter to validate email format

--- src/sigic_geonode/sigic_auth/account_adapters.py
+++ src/sigic_geonode/sigic_auth/account_adapters.py
@@ -1,10 +1,11 @@
 import logging
-
+import re
 from allauth.account.utils import user_username
 from django.core.exceptions import ValidationError
 from geonode.people.adapters import GenericOpenIDConnectAdapter, LocalAccountAdapter
 
 logger = logging.getLogger(__name__)
+EMAIL_REGEX = re.compile(r".+@.+")
 
 
 class SigicLocalAccountAdapter(LocalAccountAdapter):
@@ -15,9 +16,15 @@ class SigicLocalAccountAdapter(LocalAccountAdapter):
             try:
                 account = user.socialaccount_set.first()
 
-                preferred_username = account.get("preferred_username")
-                if preferred_username:
-                    user_username(user, preferred_username)
+                preferred_username = account.extra_data.get("preferred_username")
+                username_from_token = (
+                    preferred_username
+                    if preferred_username and EMAIL_REGEX.match(preferred_username)
+                    else data.get("email") or data.get("username")
+                )
+
+                if username_from_token:
+                    user_username(user, username_from_token)
                     return
             except Exception as e:
                 logger.error("Error al recuperar preferred_username: %s", e)
@@ -38,8 +45,9 @@ class SigicLocalAccountAdapter(LocalAccountAdapter):
 class SigicOpenIDConnectAdapter(GenericOpenIDConnectAdapter):
     def complete_login(self, request, app, token, response, **kwargs):
         login = super().complete_login(request, app, token, response, **kwargs)
-        preferred_username = login.account.get("preferred_username")
-        if preferred_username:
-            login.user.username = preferred_username
-            login.account.user = login.user
+        preferred_username = login.account.extra_data.get("preferred_username")
+        if not preferred_username or not re.match(r".+@.+", preferred_username):
+            preferred_username = data.get("email") or data.get("username")
+        login.user.username = preferred_username
+        login.account.user = login.user
         return login
