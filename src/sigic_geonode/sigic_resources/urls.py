73dcd188c137430cdd1c0cf68c0696e7f726dced
César Benjamín <mathereall@gmail.com>
2025-11-19 20:21:19 -0600

    refactor: comment out unused URL imports and router inclusion in urls.py

--- src/sigic_geonode/sigic_resources/urls.py
+++ src/sigic_geonode/sigic_resources/urls.py
@@ -1,9 +1,10 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+
+from sigic_geonode.router import router
 
 from .views import SigicResourceBaseViewSet, SigicResourceShortViewSet
 
-router = DefaultRouter()
 router.register(
     r"sigic-resources", SigicResourceBaseViewSet, basename="sigic-resources"
 )
@@ -12,7 +13,8 @@ router.register(
     SigicResourceShortViewSet,
     basename="sigic-resources-short",
 )
+
 # la url :/api/v2/resources/ mantiene la lógica original de ResourceBaseViewSet
 urlpatterns = [
-    path("", include(router.urls)),
+    # path("", include(router.urls)),
 ]
