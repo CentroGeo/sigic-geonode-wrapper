1b8046e018bf1bdabec081f2b039c57b99d571e5
andres-geotec <andres.geotec@gmail.com>
2026-02-03 23:11:35 -0600

    fix(solicitudes): cambia la publicación y la aprobación del recurso de acuerdo a las respuesta de la solicitud

--- src/sigic_geonode/sigic_requests/views.py
+++ src/sigic_geonode/sigic_requests/views.py
@@ -1,6 +1,7 @@
 import logging
 from dynamic_rest.viewsets import DynamicModelViewSet
 from rest_framework import permissions
+from rest_framework.response import Response
 from geonode.base.api.pagination import GeoNodeApiPagination
 from .models import Requests as SigicRequests
 from geonode.base.models import ResourceBase
@@ -35,12 +36,26 @@ class RequestsViewSet(DynamicModelViewSet):
         print(f"Nuevo estado de la solicitud: {status_recibido}")
         publicar = serializer.validated_data.get('status') == 'published'
         print(f"Publicar recurso: {publicar}")
+        serializer.save(reviewer=self.request.user)
 
         recurso = ResourceBase.objects.filter(id=id_recurso).first()
         recurso.is_published = publicar
+        recurso.is_approved = publicar
         recurso.save()
 
-        serializer.save(reviewer=self.request.user)
+        # Refrescar la instancia del recurso relacionado para que el serializer incluya los cambios
+        serializer.instance.resource.refresh_from_db()
+
+    # def update(self, request, *args, **kwargs):
+    #     partial = kwargs.pop('partial', False)
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_update(serializer)
+
+    #     # Refrescar el serializer con la instancia actualizada para incluir cambios en el recurso relacionado
+    #     serializer = self.get_serializer(instance)
+    #     return Response(serializer.data)
 
     def get_serializer_class(self):
         # si el usuario es admin o revisor usar el serializer para reviewer
