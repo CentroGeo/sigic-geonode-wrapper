048c4a96dd9a4a096cf8b05e835b1331718914a4
Vicente Leonardo <vicente.leo.fo@gmail.com>
2026-06-22 18:07:12 -0600

    Se agrega el endpoint para el flujo de reedicion de capas, de la mano con feat/e1-ac09-habilitar-reedicion en el frontend

--- src/sigic_geonode/sigic_requests/urls.py
+++ src/sigic_geonode/sigic_requests/urls.py
@@ -3,5 +3,6 @@ from sigic_geonode.sigic_requests.views import RequestsViewSet
 
 urlpatterns = [
     path("/", RequestsViewSet.as_view({"get": "list","post": "create"}), name="requests"),
+    path("/reopen", RequestsViewSet.as_view({"post": "reopen"}), name="request-reopen"),
     path("/<int:pk>", RequestsViewSet.as_view({"get": "retrieve","put": "update", "patch": "partial_update"}), name="request"),
 ]
